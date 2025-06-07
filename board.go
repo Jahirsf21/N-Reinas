a18e9a1244a0c8d86d4478d50e432cb9aaba7d59
Erincam09 <159506921+Erincam09@users.noreply.github.com>
2025-06-07 15:55:52 -0600

    Creacion Algoritmo Las Vegas
    Creación de algoritmo N-Reinas, funciones para reescribir y validar entradas permitidas
    
    Co-Authored-By: Braslyvm <125586293+braslyvm@users.noreply.github.com>

--- board.go
+++ board.go
@@ -1,6 +1,5 @@
 package main
 
-
 func (a *App) CreateBoard(size int) [][]int {
 	var board [][]int
 	for range size {
@@ -13,13 +12,30 @@ func (a *App) CreateBoard(size int) [][]int {
 	return board
 }
 
+func Reescribir(matrix [][]int, areglo [][]int) [][]int {
+	for _, cordenadas := range areglo {
+		x := cordenadas[0]
+		y := cordenadas[1]
+		matrix[x][y] = 2
 
-func (a *App) AlgoritmoNReinas(board [][]int, queens int) {
-
+	}
+	return matrix
 }
 
+func EntradasPermitidas(matrix [][]int) [][]int {
+	var lista [][]int
 
-func (a *App) ValidarMovimiento(board [][]int, row, col int) [][]int {
+	for x := range matrix {
+		for y := range matrix[x] {
+			if matrix[x][y] == 0 {
+				lista = append(lista, []int{x, y})
+			}
+		}
+	}
+	return lista
+}
+
+func ValidarMovimiento(board [][]int, row, col int) [][]int {
 	size := len(board)
 	posiciones := [][]int{}
 
@@ -47,10 +63,39 @@ func (a *App) ValidarMovimiento(board [][]int, row, col int) [][]int {
 		posiciones = append(posiciones, []int{i, j})
 	}
 
-
 	for i, j := row+1, col-1; i < size && j >= 0; i, j = i+1, j-1 {
 		posiciones = append(posiciones, []int{i, j})
 	}
-	
+
 	return posiciones
+}
+
+func (a *App) AlgoritmoNReinas(board [][]int, queens int) (bool, [][]int) {
+	board[0][0] = 1
+	return AlgoritmoNReinas_Aux(board, queens)
+}
+
+func AlgoritmoNReinas_Aux(board [][]int, queens int) (bool, [][]int) {
+	if queens == 0 {
+		return true, board
+	}
+
+	var posibles = EntradasPermitidas(board)
+
+	for _, posicion := range posibles {
+		var x, y = posicion[0], posicion[1]
+
+		var nuevoTablero = board
+		nuevoTablero[x][y] = 1
+
+		var bloqueos = ValidarMovimiento(nuevoTablero, x, y)
+		nuevoTablero = Reescribir(nuevoTablero, bloqueos)
+
+		var cumplido, res = AlgoritmoNReinas_Aux(nuevoTablero, queens-1)
+		if cumplido {
+			return true, res
+		}
+	}
+
+	return false, board
 }
